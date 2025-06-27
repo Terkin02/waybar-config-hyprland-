99d0d5ed96573a1ebefa2a39cecf27a6bfa058d9
Terkin02 <terk1nterraria10@gmail.com>
2025-06-28 00:14:34 +0300

    Delete hypr.sh

--- hypr.sh
+++
@@ -1,6 +0,0 @@
-sudo rm -rf ~/.config/waybar
-sudo mkdir ~/.config/waybar
-sudo mv config.jsonc ~/.config/waybar
-sudo mv style.css ~/.config/waybar
-sudo mv modules.json ~/.config/waybar
-hyprctl reload
