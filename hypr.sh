899436bda93b3f8d22f018983090bfd02b04c033
Terkin02 <terk1nterraria10@gmail.com>
2025-06-28 13:39:06 +0300

    Update hypr.sh

--- hypr.sh
+++ hypr.sh
@@ -1,6 +1,7 @@
-sudo rm -rf ~/.config/waybar
-sudo mkdir ~/.config/waybar
-sudo mv config.jsonc ~/.config/waybar
-sudo mv style.css ~/.config/waybar
-sudo mv modules.json ~/.config/waybar
+sudo su
+rm -rf ~/.config/waybar
+mkdir ~/.config/waybar
+mv config.jsonc ~/.config/waybar
+mv style.css ~/.config/waybar
+mv modules.json ~/.config/waybar
 hyprctl reload
